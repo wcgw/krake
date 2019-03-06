4231ccbee85b85061664a9386734a3762e22acd8
Alex Snaps <alex@wcgw.dev>
2019-03-05 19:46:49 -0500

    Refactor: extracted get_device fn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate libusb;
 
+use libusb::Device;
 use std::time::Duration;
 
 const VID_NZXT: u16 = 0x1e71;
@@ -28,53 +29,55 @@ fn main() {
   if devices.len() > 0 {
     for device in devices {
       let device_desc = device.device_descriptor().unwrap();
+      let mut usb_device = get_device(&device, timeout);
 
       match device_desc.product_id() {
-        PID_KRAKEN_X62 => println!(
-          "Found NZXT Kraken X62: Bus {:03} Device {:03}",
-          device.bus_number(),
-          device.address()
-        ),
+        PID_KRAKEN_X62 => {
+          println!(
+            "Found NZXT Kraken X62: Bus {:03} Device {:03}",
+            device.bus_number(),
+            device.address()
+          );
+        }
         PID_H500I => println!(
           "Found NZXT H500i controller: Bus {:03} Device {:03}",
           device.bus_number(),
           device.address()
         ),
-        _ => {
-          let mut usb_device = {
-            match device.open() {
-              Ok(h) => match h.read_languages(timeout) {
-                Ok(l) => {
-                  if l.len() > 0 {
-                    Some(UsbDevice {
-                      handle: h,
-                      language: l[0],
-                      timeout,
-                    })
-                  } else {
-                    None
-                  }
-                }
-                Err(_) => None,
-              },
-              Err(_) => None,
-            }
-          };
-          println!(
-            "Found unknown NZXT Device:{:04x} (iProduct: {:3} [{}])  at Bus {:03} Device {:03}",
-            device_desc.vendor_id(),
-            device_desc.product_string_index().unwrap_or(0),
-            usb_device.as_mut().map_or(String::new(), |h| h
-              .handle
-              .read_product_string(h.language, &device_desc, h.timeout)
-              .unwrap_or(String::new())),
-            device.bus_number(),
-            device.address()
-          )
-        }
+        _ => println!(
+          "Found unknown NZXT Device:{:04x} (iProduct: {:3} [{}])  at Bus {:03} Device {:03}",
+          device_desc.vendor_id(),
+          device_desc.product_string_index().unwrap_or(0),
+          usb_device.as_mut().map_or(String::new(), |h| h
+            .handle
+            .read_product_string(h.language, &device_desc, h.timeout)
+            .unwrap_or(String::new())),
+          device.bus_number(),
+          device.address()
+        ),
       }
     }
   } else {
     println!("No NZXT devices found!");
+  }
+}
+
+fn get_device<'a>(device: &'a Device, timeout: Duration) -> Option<UsbDevice<'a>> {
+  match device.open() {
+    Ok(handle) => match handle.read_languages(timeout) {
+      Ok(l) => {
+        if l.len() > 0 {
+          Some(UsbDevice {
+            handle,
+            language: l[0],
+            timeout,
+          })
+        } else {
+          None
+        }
+      }
+      Err(_) => None,
+    },
+    Err(_) => None,
   }
 }
