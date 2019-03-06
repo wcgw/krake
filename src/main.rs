7141ed343b00290a40264cf2b5e7b0ac149c6697
Alex Snaps <alex@wcgw.dev>
2019-03-05 20:46:08 -0500

    Let the work begin!

--- src/main.rs
+++ src/main.rs
@@ -21,16 +21,48 @@ fn main() {
     Some(command) => match command.as_str() {
       "version" => println!("Krake v0.0.1 - Controls for NZXT bells & whistles"),
       "list" => list_nzxt_devices(),
+
+      // Probably want to identify a device here first...
+      // Not sure how this is best done tho? If only one, easy... if only one of one type?
+      // A config file where one gets to id a device based of it's address and type?
+      // e.g. `gpu` is the cooler on my graphic card? `case` is the Smart Device of my H500i?
+      "leds" => match std::env::args().nth(2) {
+        // Then, per action, ask the device if it known about e.g. the color scheme for LEDs?
+        // Or default to all if not specified (works for `off, but what about e.g. `candle`?)
+        // Only apply to the ones that resolved? Only error in case none resolved?
+        // So that: `$ krake leds candle` would only affect the case, no-ops on Kraken
+        // while: `$ krake leds breathing` would apply to both, so would `leds off`
+        Some(state) => match state.as_str() {
+          "on" => leds_on(),
+          "off" => leds_off(),
+          _ => {
+            println!("Unknown state '{}' for LEDs!", state);
+            exit(1);
+          }
+        },
+        None => {
+          println!("What state for your LEDs? (on|off)");
+          exit(1);
+        }
+      },
       _ => {
         println!("Unsupported command: {}", command);
         exit(1);
       }
     },
     None => {
-      println!("Please provide a command: list, version");
+      println!("Please provide a command: (list|leds|version)");
       exit(1);
     }
   }
+}
+
+fn leds_off() -> () {
+  println!("LEDs off!")
+}
+
+fn leds_on() -> () {
+  println!("LEDs on!")
 }
 
 fn list_nzxt_devices() -> () {
